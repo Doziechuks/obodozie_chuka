b4058c8982072efba2cea4dfe0ba9c61480dc78f
doziechuks <doziechuks1010@gmail.com>
2022-10-31 07:47:37 +0100

    added transition on my mobile navbar slide

--- src/components/mobileNavbar.js
+++ src/components/mobileNavbar.js
@@ -17,32 +17,28 @@ const MobileNavbar = () => {
         <div className="mobile-nav-header">
           <RotatingName />
           <div className="handbugger-icon" onClick={toggleLink}>
-            {showLink ? (
-              <BsArrowBarUp className="bar" />
-            ) : (
-              <FaBars className="bar" />
-            )}
+            <BsArrowBarUp className={`bar ${!showLink && "remove-icon"}`} />
+
+            <FaBars className={`bar ${showLink && "remove-icon"}`} />
           </div>
         </div>
-        {showLink && (
-          <div className={`mobile-link-container ${showLink && "show-link"}`}>
-            <Link to="/" onClick={toggleLink}>
-              home
-            </Link>
-            <hr className="mobile-rule" />
-            <Link to="/about" onClick={toggleLink}>
-              about me
-            </Link>
-            <hr className="mobile-rule" />
-            <Link to="/projects" onClick={toggleLink}>
-              my projects
-            </Link>
-            <hr className="mobile-rule" />
-            <Link to="/contact" onClick={toggleLink}>
-              contact me
-            </Link>
-          </div>
-        )}
+        <div className={`mobile-link-container ${showLink && "show-link"}`}>
+          <Link to="/" onClick={toggleLink}>
+            home
+          </Link>
+          <hr className="mobile-rule" />
+          <Link to="/about" onClick={toggleLink}>
+            about me
+          </Link>
+          <hr className="mobile-rule" />
+          <Link to="/projects" onClick={toggleLink}>
+            my projects
+          </Link>
+          <hr className="mobile-rule" />
+          <Link to="/contact" onClick={toggleLink}>
+            contact me
+          </Link>
+        </div>
       </div>
     </div>
   );
